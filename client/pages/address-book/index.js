c7e180c992dcc9c5f336287aea0eee55e81985ad
tedchen529 <teddychen0529@gmail.com>
2023-12-21 14:02:13 +0800

    Add pagination - part 2

--- client/pages/address-book/index.js
+++ client/pages/address-book/index.js
@@ -1,18 +1,28 @@
 import { useEffect, useState } from "react";
 import { Layout1 } from "@/components/Layout1";
 import { AB_LIST } from "@/components/my-const";
+import { useRouter } from "next/router";
 import Link from "next/link";
+import dayjs from "dayjs";
 
 export default function ABIndex() {
   // console.log("window.location.href:", window.location.href);
   // Q: Why is window not defined? 要避免使用 window 底下的物件? Why? See pages/index.js
 
   const [data, setData] = useState({});
+  const router = useRouter();
 
   const getListData = async () => {
+    console.log("router.query", router.query);
+    let page = +router.query.page || 1;
+    // NOTE: The two "pages" are not the same
+    // Q: What decides "?page="? Test it out
+    if (page < 1) page = 1;
+
     try {
-      const r = await fetch(AB_LIST);
+      // const r = await fetch(AB_LIST);
       // NOTE: `${process.env.API_SERVER2}/address-book/api` if I set env in next.config.js
+      const r = await fetch(AB_LIST + `?page=${page}`);
       const d = await r.json();
       setData(d);
     } catch (ex) {
@@ -22,7 +32,7 @@ export default function ABIndex() {
 
   useEffect(() => {
     getListData();
-  }, []);
+  }, [router.query.page]);
   // NOTE: Because getListData is an async function and a side effect
 
   return (
@@ -30,23 +40,38 @@ export default function ABIndex() {
       <Layout1>
         {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
         {/* NOTE: JSON-formatted string */}
+
+        <div className="row">
+          <pre className="col">{router.query.page}</pre>
+        </div>
+
         <div className="row">
           <div className="col">
             <nav aria-label="Page navigation example">
               <ul className="pagination">
                 {data.success && data.totalPages
-                  ? Array(data.totalPages)
+                  ? Array(3)
                       .fill(1)
                       .map((v, i) => {
+                        const p = data.page - 1 + i;
+                        if (p < 1 || p > data.totalPages) return null;
                         return (
-                          <li key={i} className="page-item">
-                            <Link className="page-link" href="?">
-                              {i + 1}
+                          <li
+                            key={p}
+                            className={
+                              p === data.page ? "page-item active" : "page-item"
+                            }
+                          >
+                          {/* NOTE: Avoid using index as key as it may be changed */}
+                            <Link className="page-link" href={"?page=" + p}>
+                              {p}
                             </Link>
+                            {/* Q: Client side naviation without full page reload? */}
                           </li>
                         );
                       })
                   : null}
+                {/* NOTE: Array(3) represents how many page indices are shown */}
                 {/* Q: href? */}
                 {/* Q: How to add pagination in ejs? See node practice */}
               </ul>
@@ -88,7 +113,7 @@ export default function ABIndex() {
                         <td>{i.name}</td>
                         <td>{i.email}</td>
                         <td>{i.mobile}</td>
-                        <td>{i.birthday}</td>
+                        <td>{dayjs(i.birthday).format("YYYY-MM-DD")}</td>
                         <td>{i.address}</td>
                         <td>
                           <Link href="/address-book/edit/">
